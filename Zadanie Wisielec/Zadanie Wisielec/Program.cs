36c9a43bed055669a4f243c35c7a6ec0c4916b0f
OlegBrede <oleg.brede@student.wsb.edu.pl>
2025-04-30 17:40:40 +0200

    Initial commit

---
+++ Zadanie Wisielec/Zadanie Wisielec/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Zadanie_Wisielec
+{
+    class WordBank
+    {
+        //przechowywanie słów
+    }
+    class Game
+    {
+        //odsłanianie liter
+        //limit błędów
+        //stan gry
+    }
+    class Player
+    {
+        //punktacja ?
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Losowanie słowa
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
